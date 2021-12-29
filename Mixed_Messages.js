157fc9884f960dc85327da439cf34613ba7e616c
ricardorm10 <93869985+ricardorm10@users.noreply.github.com>
2021-12-29 13:55:30 -0600

    Components added

---
+++ Mixed_Messages.js
@@ -0,0 +1,5 @@
+let components = {
+    begining: ['Once upon a time a', 'Some time ago a', 'In a galaxy far far away a'],
+    adjective: ['handsome', 'dreaded', 'heroic'],
+    subjects: ['chicken', 'human', 'wookie' ]
+};
