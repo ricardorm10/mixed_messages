eb0d1545a4bc937f3abd7b41bb681a16dee84e06
ricardorm10 <93869985+ricardorm10@users.noreply.github.com>
2021-12-29 18:59:54 -0600

    1. Add array to have where to push the new phrase. 2. Add a function to create a random number. 3. Add a loop and switch. 4. Logged the output.

--- Mixed_Messages.js
+++ Mixed_Messages.js
@@ -1,5 +1,33 @@
-let components = {
-    begining: ['Once upon a time a', 'Some time ago a', 'In a galaxy far far away a'],
-    adjective: ['handsome', 'dreaded', 'heroic'],
-    subjects: ['chicken', 'human', 'wookie' ]
-};
+//Creating elements for the phrase.
+const components = {
+    looks: ['really handsome.', 'taller than yesterday.', 'absolutely amazing.'],
+    smells: ['a pack of lions.', 'the most beautiful flower.', 'mustard.'],
+    remind: ['Usain Bolt.', 'Robert De Niro.', 'Chewbacca.' ]
+}
+
+//Array for where to push the phrase.
+let phrase = []
+
+//Function to create a random number.
+let randomNumber = (num) => {
+    return Math.floor(Math.random() * num)
+}
+
+//Loop over the properties of the object.
+for(let prop in components) {
+    switch(prop){
+        case 'looks':
+            phrase.push(`You are ${components[prop][randomNumber(components.looks.length)]}`)
+            break;
+        case 'smells':
+            phrase.push(`You smell like ${components[prop][randomNumber(components.smells.length)]}`)
+            break;
+        case 'remind':
+            phrase.push(`You remind me of ${components[prop][randomNumber(components.remind.length)]}`)
+            break;
+        default:
+            phrase.push('Not enough information.')
+    }
+}
+//Logging the pushed array.
+console.log(phrase);
